14275da7eac98c8abe068bd1452bb9bda57d7a66
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2022-06-11 10:21:35 -0400

    commit tree 5cff0b7fa65629907cce8358ef8688cfb66a1f4a

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph){
+    return (cats.splice(3, 0, "Ralph"));
+
+}
+function destructivelyPrependCat(Bob){
+    return (cats.splice(0, 0, "Bob"));
+}
+function destructivelyRemoveLastCat(){
+    return (cats.splice(2));
+}
+
+function destructivelyRemoveFirstCat(){
+    return (cats.splice(0,1));
+}
+function appendCat(Broom){
+    return ([...cats, "Broom"]);
+}
+function prependCat(Arnold){
+    return (["Arnold", ...cats]);
+}
+function removeLastCat(){
+    return (cats.slice(0, 2));
+}
+
+function removeFirstCat(){
+    return (cats.slice(1, 3));
+}
+
+
+
+
+
